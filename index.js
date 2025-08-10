e5efb9d64a6fbec87f6a15b3998a333a43dad2d7
Andriy <andygrigs88@gmail.com>
2025-08-10 14:35:11 +0200

    prepearing to test hosting

--- index.js
+++ index.js
@@ -77,17 +77,24 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(errorHandler);
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://sb1d2sqww-i3ef--5173--10996a95.local-credentialless.webcontainer.io",
-    ],
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "http://localhost:5174",
+//       "https://sb1d2sqww-i3ef--5173--10996a95.local-credentialless.webcontainer.io",
+//     ],
 
-    credentials: true,
-  })
-);
+//     credentials: true,
+//   })
+// );
+
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' 
+    ? process.env.FRONTEND_URL 
+    : "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use("/uploads", express.static(path.resolve("uploads")));
 
