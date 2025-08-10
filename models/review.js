29afbd72c73e2352ff42f3bb4b302a4ba895058a
Andriy <andygrigs88@gmail.com>
2025-08-10 11:04:04 +0200

    fix review model

--- models/review.js
+++ models/review.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const reviewSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   rating: { type: Number, required: true, min: 1, max: 5 },
   comment: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
